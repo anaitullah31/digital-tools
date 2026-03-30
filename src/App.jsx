ec11889917f144eaab671bf4499567fde62bffa6
anaitullah31 <anaitullah198@gmail.com>
2026-03-30 14:05:56 +0800

    Desing header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
-      
+      <Header />
+      <div className="bg-[#101727] text-white">
+        <Footer />
+      </div>
     </>
   );
 }
---
+++ src/components/Common/PrimaryButton.jsx
@@ -0,0 +1,5 @@
+const PrimaryButton = ({ children }) => {
+  return <button className="btn btn-active btn-primary">{children}</button>;
+};
+
+export default PrimaryButton;
